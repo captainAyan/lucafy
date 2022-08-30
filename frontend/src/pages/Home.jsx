47127c6747325dcc36c6fa7e4cd4205eb261f625
captainAyan <ayanchakraborty105@gmail.com>
2022-08-31 02:42:29 +0530

    feat: :lipstick: updated template for home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Home() {
@@ -15,7 +15,28 @@ export default function Home() {
 
   return (
     <div className="w-full max-w-sm sm:mt-4">
-      <h1 className="text-4xl font-bold text-left mb-8">Home</h1>
+      <h1 className="text-4xl font-bold text-left mb-4">Home</h1>
+      <h1 className="text-2xl font-thin text-left">
+        Welcome back,{" "}
+        <span className="font-bold capitalize">{user && user.firstName}</span>
+      </h1>
+      <div className="stats mt-4 w-full">
+        <div className="stat">
+          <div className="stat-title">Assets</div>
+          <div className="stat-value">₹ 9,400</div>
+          <div className="stat-desc">Total Assets</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">Net Income</div>
+          <div className="stat-value">₹ 1,500</div>
+          <div className="stat-desc">Income less Expenditures</div>
+        </div>
+      </div>
+
+      <Link to="/entry">
+        <button className="btn btn-accent w-full mt-4">Create Entry</button>
+      </Link>
     </div>
   );
 }
