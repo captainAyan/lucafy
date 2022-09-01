9d6d2117c3304210ac3734f8a4561f9e067aabcb
captainAyan <ayanchakraborty105@gmail.com>
2022-09-02 02:32:33 +0530

    feat: :sparkles: added entry creation feature

--- frontend/src/pages/CreateEntry.jsx
+++ frontend/src/pages/CreateEntry.jsx
@@ -1,4 +1,83 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { CREATE_ENTRY_URL } from "../constants/api";
+
 export default function CreateEntry() {
+  const navigate = useNavigate();
+
+  const { ledgers, gotAll } = useSelector((state) => state.ledger);
+  const { user } = useSelector((state) => state.auth);
+
+  const [formData, setFormData] = useState({
+    debit_ledger_id: ledgers.length > 0 ? ledgers[0].id : "",
+    credit_ledger_id: ledgers.length > 0 ? ledgers[0].id : "",
+    amount: 0,
+    narration: "",
+  });
+  const { debit_ledger_id, credit_ledger_id, amount, narration } = formData;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [helperText, setHelperText] = useState("");
+  const [saveButtonLabel, setSaveButtonLabel] = useState("Save");
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+
+    if (gotAll) {
+      /**
+       * In case the user directly opens up this page and the ledgers are not
+       * saved in the redux store yet
+       */
+      setFormData((prevState) => ({
+        ...prevState,
+        debit_ledger_id: ledgers.length > 0 ? ledgers[0].id : "",
+        credit_ledger_id: ledgers.length > 0 ? ledgers[0].id : "",
+      }));
+    }
+
+    return () => {};
+  }, [user, navigate, gotAll, ledgers]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    const data = {
+      debit_ledger_id,
+      credit_ledger_id,
+      amount,
+      narration,
+    };
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+
+    try {
+      await axios.post(CREATE_ENTRY_URL, data, config);
+
+      setIsLoading(false);
+      setSaveButtonLabel("Saved 🎉");
+      setHelperText("");
+    } catch (e) {
+      setIsLoading(false);
+      setSaveButtonLabel("Save");
+      setHelperText(e.response.data.error.message);
+    }
+  };
+
   return (
     <div className="card w-full max-w-sm bg-base-100 sm:mt-24">
       <div className="card-body sm:w-96 w-full">
@@ -9,11 +88,19 @@ export default function CreateEntry() {
           <label className="label">
             <span className="label-text">Debit</span>
           </label>
-          <select className="select select-bordered">
-            <option value="1">Cash A/c</option>
-            <option value="2">Salary A/c</option>
-            <option value="3">Expenses A/c</option>
-            <option value="4">Pocket Money A/c</option>
+          <select
+            className="select select-bordered capitalize"
+            name="debit_ledger_id"
+            onChange={onChange}
+            value={debit_ledger_id}
+          >
+            {ledgers.map((item) => {
+              return (
+                <option value={item.id} key={item.id}>
+                  {item.name} A/c
+                </option>
+              );
+            })}
           </select>
         </div>
 
@@ -21,11 +108,19 @@ export default function CreateEntry() {
           <label className="label">
             <span className="label-text">Credit</span>
           </label>
-          <select className="select select-bordered">
-            <option value="1">Cash A/c</option>
-            <option value="2">Salary A/c</option>
-            <option value="3">Expenses A/c</option>
-            <option value="4">Pocket Money A/c</option>
+          <select
+            className="select select-bordered capitalize"
+            name="credit_ledger_id"
+            onChange={onChange}
+            value={credit_ledger_id}
+          >
+            {ledgers.map((item) => {
+              return (
+                <option value={item.id} key={item.id}>
+                  {item.name} A/c
+                </option>
+              );
+            })}
           </select>
         </div>
 
@@ -37,6 +132,9 @@ export default function CreateEntry() {
             type="number"
             placeholder="Amount"
             className="input input-bordered"
+            value={amount}
+            name="amount"
+            onChange={onChange}
           />
         </div>
 
@@ -47,15 +145,25 @@ export default function CreateEntry() {
           <textarea
             className="textarea textarea-bordered"
             placeholder="Narration"
+            value={narration}
             maxLength={200}
+            name="narration"
+            onChange={onChange}
           ></textarea>
           <label className="label">
-            <span className="label-text-alt">(0/200)</span>
+            <span className="label-text-alt">({narration.length}/200)</span>
           </label>
         </div>
 
-        <div className="form-control mt-4">
-          <button className="btn btn-primary">Submit</button>
+        <p className="text-red-500 text-sm text-left">{helperText}</p>
+
+        <div className="form-control mt-2">
+          <button
+            className={`btn btn-primary ${isLoading ? "loading" : ""}`}
+            onClick={handleSubmit}
+          >
+            {saveButtonLabel}
+          </button>
         </div>
       </div>
     </div>
