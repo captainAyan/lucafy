f9e13c062abc6bda328a104436744fce55e61850
captainAyan <ayanchakraborty105@gmail.com>
2022-09-03 01:51:38 +0530

    feat: :sparkles: added journal view

--- frontend/src/components/Entry.jsx
+++ frontend/src/components/Entry.jsx
@@ -1,29 +1,36 @@
 import { Link } from "react-router-dom";
 
 export default function Entry(props) {
-  const { _id, debit, credit, amount, narration } = props;
-  const time = new Date().toLocaleDateString("en-GB");
+  const {
+    id,
+    debit_ledger: debit,
+    credit_ledger: credit,
+    amount,
+    narration,
+    created_at,
+  } = props;
+  const time = new Date(created_at).toLocaleDateString("en-GB");
 
   return (
     <div className="card w-full max-w-sm bg-base-100 mb-4">
       <div className="card-body sm:w-96 w-full text-left py-4 px-6">
-        <h1 className="text-2xs font-thin break-all capitalize">
-          <Link to={`/entry/${_id}`} className="link text-blue-500">
-            #{_id}
+        <h1 className="text-2xs font-thin break-all uppercase">
+          <Link to={`/entry/${id}`} className="link text-blue-500">
+            #{id}
           </Link>
           <span className="ml-2">{time}</span>
         </h1>
 
         <div className="grid grid-rows-2 grid-flow-col">
           <div className="col-span-1 row-span-1">
-            <Link to={`/ledger/${debit._id}`}>
+            <Link to={`/ledger/${debit._id}`} title={debit.description}>
               <h1 className="text-xl font-bold break-all capitalize">
                 {debit.name} A/c
               </h1>
             </Link>
           </div>
           <div className="col-span-1 mt-1">
-            <Link to={`/ledger/${credit._id}`}>
+            <Link to={`/ledger/${credit._id}`} title={credit.description}>
               <h1 className="text-2lg font-thin break-all capitalize">
                 {credit.name} A/c
               </h1>
--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -1,48 +1,95 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+
 import Entry from "../components/Entry";
+import { GET_ENTRY_URL } from "../constants/api";
 
 export default function Journal() {
-  const cash = { _id: "1", name: "cash" };
-  const salary = { _id: "2", name: "salary" };
-  const pocketName = { _id: "3", name: "pocket name" };
-  const expenses = { _id: "4", name: "expenses" };
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [entries, setEntries] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const [searchParams] = useSearchParams();
+  const [page, setPage] = useState(parseInt(searchParams.get("page")) || 1);
+
+  useEffect(() => {
+    getJournal(page);
+    navigate(`?page=${page}`);
+  }, [page]);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+
+    return () => {};
+  }, [user, navigate]);
+
+  const getJournal = async (page) => {
+    setIsLoading(true);
+    const query = new URLSearchParams({ page: page - 1 });
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+
+    try {
+      const response = await axios.get(`${GET_ENTRY_URL}?${query}`, config);
+      setEntries(response.data.entries);
+      const { limit, total } = response.data;
+      setTotalPages(Math.ceil(total / limit));
+      setIsLoading(false);
+    } catch (e) {
+      setEntries([]);
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="w-full max-w-sm sm:mt-4">
-      <h1 className="text-4xl font-bold text-left mb-8">Journal</h1>
+      <h1 className="text-4xl font-bold text-left mb-2">Journal</h1>
+      <p className="text-sm text-left mb-4">
+        Page <span>{page}</span> of <span>{totalPages}</span>
+      </p>
 
-      <Entry
-        _id="1"
-        debit={cash}
-        credit={salary}
-        amount="1,000.00"
-        narration="The href attribute is required for an anchor to be keyboard accessible."
-      />
-      <Entry
-        _id="2"
-        debit={expenses}
-        credit={cash}
-        amount="170.00"
-        narration="Being amount spent on Rakshabandhan gift."
-      />
-      <Entry
-        _id="3"
-        debit={cash}
-        credit={pocketName}
-        amount="258.00"
-        narration="Being pocket money received."
-      />
-      <Entry
-        _id="4"
-        debit={expenses}
-        credit={cash}
-        amount="992.00"
-        narration="Being amount spent on chest bag."
-      />
+      <div className="btn-group w-full max-w-sm mb-4">
+        <button className="btn" onClick={() => setPage(page - 1)}>
+          «
+        </button>
+        <button className="btn">Page {page}</button>
+        <button className="btn" onClick={() => setPage(page + 1)}>
+          »
+        </button>
+      </div>
+
+      {isLoading ? (
+        <progress className="progress w-full progress-primary bg-base" />
+      ) : null}
+
+      {entries.map((entry) => {
+        return <Entry {...entry} key={entry.id} />;
+      })}
+
+      {!isLoading && entries.length === 0 ? (
+        <h1 className="text-2xl font-thin text-left mb-4">No Data</h1>
+      ) : null}
 
       <div className="btn-group w-full max-w-sm">
-        <button className="btn">«</button>
-        <button className="btn">Page 22</button>
-        <button className="btn">»</button>
+        <button className="btn" onClick={() => setPage(page - 1)}>
+          «
+        </button>
+        <button className="btn">Page {page}</button>
+        <button className="btn" onClick={() => setPage(page + 1)}>
+          »
+        </button>
       </div>
     </div>
   );
