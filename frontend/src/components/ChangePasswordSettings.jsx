c59077a8e37501b7bc511b5ce1ed3077db7e3e69
captainAyan <ayanchakraborty105@gmail.com>
2023-04-24 20:36:23 +0530

    refactor: :recycle: reimplemented change password form using formik and added yup validation

--- frontend/src/components/ChangePasswordSettings.jsx
+++ frontend/src/components/ChangePasswordSettings.jsx
@@ -1,47 +1,36 @@
-import axios from "axios";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { Formik, Form, ErrorMessage, Field } from "formik";
+
 import { CHANGE_PASSWORD_URL } from "../constants/api";
 import authConfig from "../util/authConfig";
-import { useSelector } from "react-redux";
+import ChangePasswordSchema from "../util/changePasswordValidationSchema";
 
 export default function ChangePasswordSettings() {
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     oldPassword: "",
     newPassword: "",
-    newPasswordConfirm: "",
-  });
-  const { oldPassword, newPassword, newPasswordConfirm } = formData;
+    confirmNewPassword: "",
+  };
   const [helperText, setHelperText] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [buttonLabel, setButtonLabel] = useState("Save");
 
   const { token } = useSelector((state) => state.auth);
 
-  const onFormChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    if (newPassword === newPasswordConfirm) {
-      setIsLoading(true);
-      axios
-        .put(
-          CHANGE_PASSWORD_URL,
-          { oldPassword, newPassword },
-          authConfig(token)
-        )
-        .then(() => {
-          setHelperText("");
-          setButtonLabel("Saved 🎉");
-        })
-        .catch((error) => {
-          setHelperText(error.response.data.error.message);
-        })
-        .finally(() => setIsLoading(false));
-    } else setHelperText("New passwords don't match");
+  const handleSubmit = (data, formResetFn) => {
+    const { oldPassword, newPassword } = data;
+    setIsLoading(true);
+    axios
+      .put(CHANGE_PASSWORD_URL, { oldPassword, newPassword }, authConfig(token))
+      .then(() => {
+        setHelperText("");
+        setButtonLabel("Saved 🎉");
+        formResetFn();
+      })
+      .catch((error) => setHelperText(error.response.data.error.message))
+      .finally(() => setIsLoading(false));
   };
 
   return (
@@ -50,56 +39,72 @@ export default function ChangePasswordSettings() {
         <div className="card-title">
           <h1 className="text-2xl font-bold">Change Password</h1>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Old Password</span>
-          </label>
-          <input
-            type="password"
-            name="oldPassword"
-            value={oldPassword}
-            onChange={onFormChange}
-            placeholder="Old Password"
-            className="input input-bordered"
-          />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">New Password</span>
-          </label>
-          <input
-            type="password"
-            name="newPassword"
-            value={newPassword}
-            onChange={onFormChange}
-            placeholder="New Password"
-            className="input input-bordered"
-          />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Confirm New Password</span>
-          </label>
-          <input
-            type="password"
-            name="newPasswordConfirm"
-            value={newPasswordConfirm}
-            onChange={onFormChange}
-            placeholder="Confirm New Password"
-            className="input input-bordered"
-          />
-        </div>
 
-        <p className="text-red-500 text-sm text-left">{helperText}</p>
+        <Formik
+          initialValues={initialFormData}
+          validationSchema={ChangePasswordSchema}
+          onSubmit={async (values, { resetForm }) => {
+            handleSubmit(values, resetForm);
+          }}
+        >
+          <Form>
+            <div className="form-control">
+              <label className="label" htmlFor="oldPassword">
+                <span className="label-text">Old Password</span>
+              </label>
+              <Field
+                type="password"
+                name="oldPassword"
+                placeholder="Old Password"
+                className="input input-bordered"
+              />
+              <span className="text-red-500 text-sm text-left">
+                <ErrorMessage name="oldPassword" />
+              </span>
+            </div>
+            <div className="form-control">
+              <label className="label" htmlFor="newPassword">
+                <span className="label-text">New Password</span>
+              </label>
+              <Field
+                type="password"
+                name="newPassword"
+                placeholder="New Password"
+                className="input input-bordered"
+              />
+              <span className="text-red-500 text-sm text-left">
+                <ErrorMessage name="newPassword" />
+              </span>
+            </div>
+            <div className="form-control">
+              <label className="label" htmlFor="confirmNewPassword">
+                <span className="label-text">Confirm New Password</span>
+              </label>
+              <Field
+                type="password"
+                name="confirmNewPassword"
+                placeholder="Confirm New Password"
+                className="input input-bordered"
+              />
+              <span className="text-red-500 text-sm text-left">
+                <ErrorMessage name="confirmNewPassword" />
+              </span>
+            </div>
 
-        <div className="form-control mt-4">
-          <button
-            className={`btn btn-info text-white ${isLoading ? "loading" : ""}`}
-            onClick={handleSubmit}
-          >
-            {buttonLabel}
-          </button>
-        </div>
+            <p className="text-red-500 text-sm text-left">{helperText}</p>
+
+            <div className="form-control mt-4">
+              <button
+                className={`btn btn-info text-white ${
+                  isLoading ? "loading" : ""
+                }`}
+                type="submit"
+              >
+                {buttonLabel}
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
