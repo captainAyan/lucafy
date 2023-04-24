5b69cf0d705e6e5913ea05b79b46cec55fabb29d
captainAyan <ayanchakraborty105@gmail.com>
2023-04-24 21:53:20 +0530

    refactor: :recycle: reimplemented change preference form using formik and added yup validation

--- frontend/src/components/PreferenceSettings.jsx
+++ frontend/src/components/PreferenceSettings.jsx
@@ -15,27 +15,22 @@ import { INDIAN, INTERNATIONAL } from "../constants/amountFormat";
 import { setPreference } from "../features/preference/preferenceSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import PreferenceSchema from "../util/preferenceValidationSchema";
 
 export default function PreferenceSettings() {
   const dispatch = useDispatch();
   const preference = useSelector((state) => state.preference);
 
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     amountFormat: preference.amountFormat,
     currency: preference.currency,
-  });
-  const { amountFormat, currency } = formData;
+  };
 
   const [buttonLabel, setButtonLabel] = useState("Save");
 
-  const onFormChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSubmit = () => {
+  const handleSubmit = (values) => {
+    const { amountFormat, currency } = values;
     const preference = { amountFormat, currency };
 
     dispatch(setPreference(preference));
@@ -49,51 +44,62 @@ export default function PreferenceSettings() {
           <h1 className="text-2xl font-bold">Preference</h1>
         </div>
 
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">Currency Format</span>
-          </label>
-          <select
-            className="select select-bordered"
-            name="amountFormat"
-            value={amountFormat}
-            onChange={onFormChange}
-          >
-            <option value={INDIAN}>Indian</option>
-            <option value={INTERNATIONAL}>International</option>
-          </select>
-        </div>
+        <Formik
+          initialValues={initialFormData}
+          validationSchema={PreferenceSchema}
+          onSubmit={async (values) => {
+            handleSubmit(values);
+          }}
+        >
+          <Form>
+            <div className="form-control w-full max-w-xs">
+              <label className="label" htmlFor="amountFormat">
+                <span className="label-text">Currency Format</span>
+              </label>
+              <Field
+                as="select"
+                className="select select-bordered"
+                name="amountFormat"
+              >
+                <option value={INDIAN}>Indian</option>
+                <option value={INTERNATIONAL}>International</option>
+              </Field>
+              <span className="text-red-500 text-sm text-left">
+                <ErrorMessage name="amountFormat" />
+              </span>
+            </div>
 
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">Currency Name</span>
-          </label>
-          <select
-            className="select select-bordered"
-            name="currency"
-            value={currency}
-            onChange={onFormChange}
-          >
-            <option value={RUPEE}>Rupee (₹) 🇮🇳</option>
-            <option value={DOLLAR}>Dollar ($) 🇺🇸 🇦🇺 🇨🇦 🇲🇽</option>
-            <option value={EURO}>Euro (€) 🇪🇺</option>
-            <option value={NAIRA}>Naira (₦) 🇳🇬</option>
-            <option value={NEW_SHEKEL}>New Shekel (₪) 🇮🇱</option>
-            <option value={POUND}>Pound (£) 🇬🇧</option>
-            <option value={RUBLE}>Ruble (₽) 🇷🇺</option>
-            <option value={TAKA}>Taka (৳) 🇧🇩</option>
-            <option value={WON}>Won (₩) 🇰🇷</option>
-            <option value={YEN}>Yen (¥) 🇯🇵 🇨🇳</option>
-          </select>
-        </div>
-        <div className="form-control mt-4">
-          <button
-            className="btn bg-green-500 text-white"
-            onClick={handleSubmit}
-          >
-            {buttonLabel}
-          </button>
-        </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label" htmlFor="currency">
+                <span className="label-text">Currency Name</span>
+              </label>
+              <Field
+                as="select"
+                className="select select-bordered"
+                name="currency"
+              >
+                <option value={RUPEE}>Rupee (₹) 🇮🇳</option>
+                <option value={DOLLAR}>Dollar ($) 🇺🇸 🇦🇺 🇨🇦 🇲🇽</option>
+                <option value={EURO}>Euro (€) 🇪🇺</option>
+                <option value={NAIRA}>Naira (₦) 🇳🇬</option>
+                <option value={NEW_SHEKEL}>New Shekel (₪) 🇮🇱</option>
+                <option value={POUND}>Pound (£) 🇬🇧</option>
+                <option value={RUBLE}>Ruble (₽) 🇷🇺</option>
+                <option value={TAKA}>Taka (৳) 🇧🇩</option>
+                <option value={WON}>Won (₩) 🇰🇷</option>
+                <option value={YEN}>Yen (¥) 🇯🇵 🇨🇳</option>
+              </Field>
+              <span className="text-red-500 text-sm text-left">
+                <ErrorMessage name="currency" />
+              </span>
+            </div>
+            <div className="form-control mt-4">
+              <button className="btn bg-green-500 text-white" type="submit">
+                {buttonLabel}
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
