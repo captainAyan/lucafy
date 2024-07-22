b78c60fa186553420a4491e0c579b72a50ab7efe
captainAyan <ayanchakraborty105@gmail.com>
2024-07-22 17:33:32 +0530

    build: :construction_worker: converted from CRA to Vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,9 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
